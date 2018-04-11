333b078a14f38da1a75d5c2165c566ed8566a084
Daniel Johnson <dajohnson1978@gmail.com>
2018-04-11 15:06:37 -0500

    removing excessive print statements

--- src/cobblestone/core.clj
+++ src/cobblestone/core.clj
@@ -179,11 +179,7 @@
                                       {:keys [name palette-name actions locs]} v
                                       actions (if (empty? actions) [] actions)
                                       {:keys [pixels]} (get tiles name)
-                                      _ (println "pixel count: " (count pixels))
-                                      _ (println "pixel: " pixels)
                                       palette (get palettes palette-name)
-                                      _ (println "palette: " palette)
-                                      _ (println (if (some nil? palette) true false))
                                       {:keys [bg pixels]} (if (some nil? palette) {:pixels pixels} (optimize-tile pixels))
                                       pixels (reduce-kv #(assoc %1 %2 (mapv (fn [pixel] (reduce do-action pixel actions)) %3)) {} pixels)
                                       id (str tile-name index)
